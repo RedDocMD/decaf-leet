54052fad566ae78c5815a1c12c4c38fd737df754
Deep Majumder <deep.majumder2019@gmail.com>
2022-04-16 11:47:34 +0530

    input box controls

--- src/components/input_box.tsx
+++ src/components/input_box.tsx
@@ -1,21 +1,28 @@
 import * as React from "react";
 
+type textCbFn = (text: string) => void;
 export interface InputBoxProps {
   rowCnt: number;
   classes?: string;
   label: string;
+  textCb: textCbFn;
 }
 
 export function InputBox(props: InputBoxProps) {
+  const onChangeCb = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    props.textCb(event.target.value);
+  };
+
   return (
     <span className={"flex-1" + ` ${props.classes}`}>
-      <div className="pb-1">{props.label}</div>
+      <div className="pb-1 text-sm">{props.label}</div>
       <div className="flex">
         <textarea
           className={
             "border-2 border-gray-300 rounded-md py-1 px-2 resize-none flex-1"
           }
           rows={props.rowCnt}
+          onChange={onChangeCb}
         />
       </div>
     </span>
--- src/components/top_level.tsx
+++ src/components/top_level.tsx
@@ -4,14 +4,32 @@ import { InputBox } from "./input_box";
 export default function TopLevel() {
   const rowCnt = 8;
 
+  const [inputText, setInputText] = React.useState("");
+  const [outputText, setOutputText] = React.useState("");
+  const [expectedText, setExpectedText] = React.useState("");
+
+  const inputCb = (text: string) => setInputText(text);
+  const outputCb = (text: string) => setOutputText(text);
+  const expectedCb = (text: string) => setExpectedText(text);
+
   return (
     <div className="container mx-auto">
       <div className="flex justify-between">
-        <InputBox rowCnt={rowCnt} label="Input" classes="mr-2" />
-        <InputBox rowCnt={rowCnt} label="Expected" classes="ml-2" />
+        <InputBox
+          rowCnt={rowCnt}
+          label="Input"
+          classes="mr-4"
+          textCb={inputCb}
+        />
+        <InputBox
+          rowCnt={rowCnt}
+          label="Expected"
+          classes="ml-4"
+          textCb={expectedCb}
+        />
       </div>
-      <div className="flex mt-4">
-        <InputBox rowCnt={rowCnt} label="Output" />
+      <div className="flex mt-3">
+        <InputBox rowCnt={rowCnt} label="Output" textCb={outputCb} />
       </div>
     </div>
   );
